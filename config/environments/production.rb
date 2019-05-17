dd26a9f8d9f641b19f194d712a92fe987df4f98c
ponpppher <genmaier@gmail.com>
2019-05-17 13:23:44 +0900

    modify toppage background-img  #21

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # enable assets files
+  config.public_file_server.enabled = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -26,7 +29,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
