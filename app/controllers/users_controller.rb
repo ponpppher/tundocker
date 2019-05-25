90edfe707b5f2960b8126b41fa986efa4e184679
ponpppher <genmaier@gmail.com>
2019-05-25 17:40:26 +0900

    modify: book group info page layout #28

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,8 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @user = User.safe_name(user)
+
+    @favs = current_user.favs
+    @article_favs = current_user.article_fav
   end
 end
