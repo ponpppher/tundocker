57e81e836ea7ca84ce681c5594b5e6dfeb439421
ponpppher <genmaier@gmail.com>
2019-06-04 16:30:49 +0900

    update ja.yml, execute squasher for compress migration file and fix favorite count didn't update #41

---
+++ db/migrate/20190512113758_init_schema.rb
@@ -0,0 +1,134 @@
+class InitSchema < ActiveRecord::Migration[5.2]
+  def up
+    # These are extensions that must be enabled in order to support this database
+    enable_extension "plpgsql"
+    create_table "article_favs" do |t|
+      t.bigint "user_id"
+      t.bigint "article_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["article_id"], name: "index_article_favs_on_article_id"
+      t.index ["user_id", "article_id"], name: "index_article_favs_on_user_id_and_article_id", unique: true
+      t.index ["user_id"], name: "index_article_favs_on_user_id"
+    end
+    create_table "articles" do |t|
+      t.string "title", null: false, comment: "記事のタイトル"
+      t.text "body", comment: "記事の本文"
+      t.bigint "book_id"
+      t.bigint "user_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["book_id"], name: "index_articles_on_book_id"
+      t.index ["user_id", "book_id"], name: "index_articles_on_user_id_and_book_id"
+      t.index ["user_id"], name: "index_articles_on_user_id"
+    end
+    create_table "books" do |t|
+      t.string "title", null: false, comment: "本のタイトル"
+      t.datetime "publish_on", comment: "出版日"
+      t.integer "sheets", comment: "ページ数"
+      t.integer "price", comment: "値段"
+      t.string "image", comment: "本のサムネイル"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+    create_table "comments" do |t|
+      t.bigint "user_id"
+      t.bigint "article_id"
+      t.text "content", comment: "コメント本文"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["article_id"], name: "index_comments_on_article_id"
+      t.index ["user_id"], name: "index_comments_on_user_id"
+    end
+    create_table "groups" do |t|
+      t.bigint "recommend_id"
+      t.bigint "book_id"
+      t.string "description", comment: "登録した本の詳細文"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["book_id"], name: "index_groups_on_book_id"
+      t.index ["recommend_id"], name: "index_groups_on_recommend_id"
+    end
+    create_table "recommend_favs" do |t|
+      t.bigint "user_id"
+      t.bigint "recommend_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["recommend_id"], name: "index_recommend_favs_on_recommend_id"
+      t.index ["user_id", "recommend_id"], name: "index_recommend_favs_on_user_id_and_recommend_id", unique: true
+      t.index ["user_id"], name: "index_recommend_favs_on_user_id"
+    end
+    create_table "recommends" do |t|
+      t.string "name", null: false, comment: "グループ名"
+      t.text "summary", null: false, comment: "概要"
+      t.bigint "user_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["user_id"], name: "index_recommends_on_user_id"
+    end
+    create_table "regist_books" do |t|
+      t.bigint "user_id"
+      t.bigint "book_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["book_id"], name: "index_regist_books_on_book_id"
+      t.index ["user_id", "book_id"], name: "index_regist_books_on_user_id_and_book_id", unique: true
+      t.index ["user_id"], name: "index_regist_books_on_user_id"
+    end
+    create_table "taggings", id: :serial do |t|
+      t.integer "tag_id"
+      t.string "taggable_type"
+      t.integer "taggable_id"
+      t.string "tagger_type"
+      t.integer "tagger_id"
+      t.string "context", limit: 128
+      t.datetime "created_at"
+      t.index ["context"], name: "index_taggings_on_context"
+      t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+      t.index ["tag_id"], name: "index_taggings_on_tag_id"
+      t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+      t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+      t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+      t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+      t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+      t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+    end
+    create_table "tags", id: :serial do |t|
+      t.string "name"
+      t.integer "taggings_count", default: 0
+      t.index ["name"], name: "index_tags_on_name", unique: true
+    end
+    create_table "users" do |t|
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.string "name", comment: "名前"
+      t.string "provider", comment: "連携サービス"
+      t.string "uid", comment: "ユニークID"
+      t.string "username", comment: "サービス登録名"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    end
+    add_foreign_key "article_favs", "articles"
+    add_foreign_key "article_favs", "users"
+    add_foreign_key "articles", "books"
+    add_foreign_key "articles", "users"
+    add_foreign_key "comments", "articles"
+    add_foreign_key "comments", "users"
+    add_foreign_key "groups", "books"
+    add_foreign_key "groups", "recommends"
+    add_foreign_key "recommend_favs", "recommends"
+    add_foreign_key "recommend_favs", "users"
+    add_foreign_key "recommends", "users"
+    add_foreign_key "regist_books", "books"
+    add_foreign_key "regist_books", "users"
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, "The initial migration is not revertable"
+  end
+end
